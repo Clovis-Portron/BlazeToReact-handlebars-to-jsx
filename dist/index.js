24b99041aa18dda84e06ad808e8e5825edb147aa
clovis <clovis@mail.byte49.exposed>
2020-04-09 11:49:57 +0200

    Modifications paramètres pour prise en charge Glimmer.Program

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable import/export */
-var syntax_1 = require("@glimmer/syntax");
 var generator_1 = require("@babel/generator");
 var program_1 = require("./program");
 var spacebarsAdapter_1 = require("./spacebarsAdapter");
+/**
+ * Converts Handlebars code to JSX code
+ * @param hbsCode Handlebars code to convert
+ * @param [options] Compilation options
+ * @param [options.isComponent] Should return JSX code wrapped as a function component
+ * @param [options.isModule] Should return generated code exported as default
+ * @param [options.includeImport] Should include react import
+ */
 function compile(hbsCode, options) {
-    if (options === void 0) { options = true; }
-    if (typeof options === 'boolean') {
-        return compile(hbsCode, { isComponent: options });
-    }
     var isComponent = !!options.isComponent;
     var isModule = !!options.isModule;
     var includeImport = !!options.includeImport && isModule;
-    //const code = preprocess(hbsCode)
-    //console.log(JSON.stringify(code, null, 2));
-    //return;
-    var glimmerProgram = spacebarsAdapter_1.adaptForSpacebars(syntax_1.preprocess(hbsCode));
-    //return JSON.stringify(glimmerProgram);
+    // const code = preprocess(hbsCode)
+    // console.log(JSON.stringify(code, null, 2));
+    // return;
+    var glimmerProgram = spacebarsAdapter_1.adaptForSpacebars(hbsCode);
+    // return JSON.stringify(glimmerProgram);
     var babelProgram = program_1.createProgram(glimmerProgram, isComponent, isModule, includeImport);
     if (options.isJSX)
         return babelProgram;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable import/export */
-import { preprocess }    from '@glimmer/syntax'
-import generate          from '@babel/generator'
-import * as Babel        from '@babel/types'
-import { createProgram } from './program'
-import { adaptForSpacebars } from './spacebarsAdapter'; 
+import { AST as Glimmer }    from '@glimmer/syntax'
+import generate              from '@babel/generator'
+import * as Babel            from '@babel/types'
+import { createProgram }     from './program'
+import { adaptForSpacebars } from './spacebarsAdapter'
 
 /**
  * Converts Handlebars code to JSX code
@@ -13,37 +13,21 @@ import { adaptForSpacebars } from './spacebarsAdapter';
  * @param [options.isModule] Should return generated code exported as default
  * @param [options.includeImport] Should include react import
  */
-export function compile(hbsCode: string, isComponent?: boolean): string
 export function compile(
-  hbsCode: string,
-  options?: {
-    isComponent?: boolean
-    isModule?: boolean
-    includeImport?: boolean
-    isJSX?: boolean
-  }
-): string
-export function compile(
-  hbsCode: string,
-  options: boolean | { isComponent?: boolean; isModule?: boolean; includeImport?: boolean; isJSX?: boolean } = true
+  hbsCode: Glimmer.Program,
+  options: { isComponent?: boolean; isModule?: boolean; includeImport?: boolean; isJSX?: boolean }
 ): string | any {
-  if (typeof options === 'boolean') {
-    return compile(hbsCode, { isComponent: options })
-  }
-
   const isComponent = !!options.isComponent
   const isModule = !!options.isModule
   const includeImport = !!options.includeImport && isModule
-  //const code = preprocess(hbsCode)
-  //console.log(JSON.stringify(code, null, 2));
-  //return;
+  // const code = preprocess(hbsCode)
+  // console.log(JSON.stringify(code, null, 2));
+  // return;
 
-  const glimmerProgram = adaptForSpacebars(preprocess(hbsCode))
-  //return JSON.stringify(glimmerProgram);
+  const glimmerProgram = adaptForSpacebars(hbsCode)
+  // return JSON.stringify(glimmerProgram);
   const babelProgram: Babel.Program = createProgram(glimmerProgram, isComponent, isModule, includeImport)
 
-  if(options.isJSX) return babelProgram
-
-
+  if (options.isJSX) return babelProgram
   return generate(babelProgram).code
 }
